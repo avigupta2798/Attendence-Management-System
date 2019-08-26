3eb2b9670183807bfba563340d23c43e45ec8753
Anshul Hudda <anshulhudda.ssap@gmail.com>
2019-08-26 09:01:44 +0530

    Changed Database Settings

--- attendence_system/settings.py
+++ attendence_system/settings.py
@@ -27,9 +27,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'NAME' : 'local',
-        'USER' : 'root',
-        'PASSWORD' : 'root',
+        'NAME' : 'attendance',
+        'USER' : 'management',
+        'PASSWORD' : 'pass',
         'HOST' : 'localhost',
         'PORT' : '3306',
     }
